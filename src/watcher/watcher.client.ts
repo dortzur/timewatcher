32084482c82b1058272d8d58cdea9f9cdf4ecce7
Dor Tzur <dortzur@gmail.com>
2020-08-07 18:34:06 +0300

    max functional, add random wait time

--- src/watcher/watcher.client.ts
+++ src/watcher/watcher.client.ts
@@ -1,60 +1,55 @@
 import axios, { AxiosInstance } from 'axios';
 import qs from 'qs';
 import * as AxiosLogger from 'axios-logger';
-import { BASE_CLIENT_PARAMS, CheckinStatus, getCheckinStatusParams, getLoginParams } from './watcher.params';
+import {
+  BASE_CLIENT_PARAMS,
+  CheckinStatus,
+  getCheckinStatusParams,
+  getLoginParams,
+} from './watcher.params';
 import { User } from './user.interface';
 import { Injectable } from '@nestjs/common';
-
 
 const authTokenRegex = /ixee: (.*)}/;
 const LOGIN_PATH = '/punch2.php';
 const CHECKIN_STATUS_PATH = '/punch3.php';
 AxiosLogger.setGlobalConfig({ headers: true });
 
-class UserCheckin {
-  constructor(user: User) {
-    this.user = user;
-    this.client = axios.create(BASE_CLIENT_PARAMS);
-    if (process.env.NODE_ENV !== 'production') {
-      this.client.interceptors.request.use(AxiosLogger.requestLogger);
-      this.client.interceptors.response.use(AxiosLogger.responseLogger);
-    }
+const createClient = () => {
+  const client = axios.create(BASE_CLIENT_PARAMS);
+  if (process.env.DEBUG_AXIOS) {
+    client.interceptors.request.use(AxiosLogger.requestLogger);
+    client.interceptors.response.use(AxiosLogger.responseLogger);
   }
 
-  readonly user: User;
+  return client;
+};
+const login = async (user: User) => {
+  const client = createClient();
+  const params = qs.stringify(getLoginParams(user));
+  const response = await client.post(LOGIN_PATH, params, {
+    withCredentials: true,
+  });
+  const cookies = response.headers['set-cookie'].join(';');
+  const result = authTokenRegex.exec(response.data);
+  if (!result || !result[1]) {
+    throw new Error('Login Failed');
+  }
+  const authToken = result[1];
+  return { client, cookies, authToken };
+};
 
-  readonly client: AxiosInstance;
-
-  #authToken: string;
-  #cookies: [string];
-  #login = async () => {
-    if (!this.user || !this.user.company || !this.user.name || !this.user.password) throw new Error('User must be fully defined');
-    const params = qs.stringify(getLoginParams(this.user));
-    const response = await this.client.post(LOGIN_PATH, params, { withCredentials: true });
-
-    this.#cookies = response.headers['set-cookie'].join(';');
-    const result = authTokenRegex.exec(response.data);
-    if (!result || !result[1]) {
-      throw new Error('Login Failed');
-    }
-    this.#authToken = result[1];
-  };
-
-  checkin = async () => this.#changeStatus(CheckinStatus.Checkin);
-  checkout = async () => this.#changeStatus(CheckinStatus.Checkout);
-
-  #changeStatus = async (status: CheckinStatus) => {
-    await this.#login();
-    const params = getCheckinStatusParams(this.user, this.#authToken, status);
-    return this.client.post(CHECKIN_STATUS_PATH, qs.stringify(params), { withCredentials: true, headers: { Cookie: this.#cookies } });
-  };
-}
-
-
+const changeCheckinStatus = async (user: User, status: CheckinStatus) => {
+  const { client, cookies, authToken } = await login(user);
+  const params = getCheckinStatusParams(user, authToken, status);
+  return client.post(CHECKIN_STATUS_PATH, qs.stringify(params), {
+    withCredentials: true,
+    headers: { Cookie: cookies },
+  });
+};
 
 @Injectable()
 export class WatcherClient {
-  checkin = (user: User) => new UserCheckin(user).checkin();
-  checkout = (user: User) => new UserCheckin(user).checkout();
+  checkin = (user: User) => changeCheckinStatus(user, CheckinStatus.Checkin);
+  checkout = (user: User) => changeCheckinStatus(user, CheckinStatus.Checkout);
 }
-
--- src/watcher/watcher.controller.ts
+++ src/watcher/watcher.controller.ts
@@ -1,20 +1,53 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { WatcherService } from './watcher.service';
 import { User } from './user.interface';
+import randomInt from 'random-int';
+
+const MINUTES_MULTIPLAYER = 1000 * 60;
+const wait = minutes =>
+  new Promise((resolve, reject) => {
+    setTimeout(resolve, minutes * MINUTES_MULTIPLAYER);
+  });
+
+const randWait = maxMinutes => {
+  const randMinutes = randomInt(0, maxMinutes);
+  return wait(randMinutes);
+};
 
 @Controller('watcher')
 export class WatcherController {
   constructor(private readonly watcherService: WatcherService) {}
 
   @Post('checkin')
-  async checkin(@Body() user: User): Promise<string> {
-    const result = await this.watcherService.checkin(user);
-    return 'checkin';
+  async checkin(
+    @Body() user: User,
+    @Query('max_wait', new DefaultValuePipe(0), new ParseIntPipe())
+    maxWait: number
+  ): Promise<string> {
+    randWait(maxWait).then(() => {
+      this.watcherService.checkin(user);
+    });
+
+    return `Great Success`;
   }
 
   @Post('checkout')
-  async checkout(@Body() user: User): Promise<string> {
-    const result = await this.watcherService.checkout(user);
-    return 'checkout';
+  async checkout(
+    @Body() user: User,
+    @Query('max_wait', new DefaultValuePipe(0), new ParseIntPipe())
+    maxWait: number
+  ): Promise<string> {
+    randWait(maxWait).then(() => {
+      this.watcherService.checkin(user);
+    });
+    await this.watcherService.checkout(user);
+    return `Great Success`;
   }
 }
